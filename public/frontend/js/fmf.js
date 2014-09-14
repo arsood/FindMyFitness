be879443d46f2cadd5b7f117e479e47f6dcb36b2
arsood <arsood@gmail.com>
2014-09-13 22:05:54 -0700

    Further work on business pages

---
+++ public/frontend/js/fmf.js
@@ -0,0 +1,4 @@
+$(document).on("click", ".gray-rollover-menu-item", function() {
+	$(".gray-rollover-menu-item").removeClass("gray-rollover-menu-item-active");
+	$(this).addClass("gray-rollover-menu-item-active");
+});
