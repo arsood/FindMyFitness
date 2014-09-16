1f8d1d1a6cd92c3c3dbababc6be9790022a9ed8f
Arun Sood <arsood@Aruns-MacBook-Pro.local>
2014-09-16 00:04:50 -0700

    Business page work

--- public/frontend/js/fmf.js
+++ public/frontend/js/fmf.js
@@ -1,4 +1,20 @@
+//Handle business page menu toggling
+
 $(document).on("click", ".gray-rollover-menu-item", function() {
 	$(".gray-rollover-menu-item").removeClass("gray-rollover-menu-item-active");
 	$(this).addClass("gray-rollover-menu-item-active");
+});
+
+//Toggle business views
+
+$(document).on("click", "#toggle-bus-info", function() {
+	$("#business-info").fadeIn();
+	$("#business-reviews").hide();
+	$("#business-photos").hide();
+});
+
+$(document).on("click", "#toggle-bus-reviews", function() {
+	$("#business-info").hide();
+	$("#business-reviews").fadeIn();
+	$("#business-photos").hide();
 });
