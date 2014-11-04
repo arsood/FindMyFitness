0eba9113fcf8e2a1818f7a2b9a838096458169fb
Arun Sood <arsood@Aruns-MacBook-Pro.local>
2014-11-04 15:06:40 -0800

    Added seeds

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,15 @@ require "bcrypt"
 
 class User < ActiveRecord::Base
 
-include BCrypt
+# include BCrypt
 
-def password
-	@password ||= Password.new(password_hash)
-end
+# def password
+# 	@password ||= Password.new(password_hash)
+# end
 
-def password=(new_password)
-	@password = Password.create(new_password)
-	self.password_hash = @password
-end
+# def password=(new_password)
+# 	@password = Password.create(new_password)
+# 	self.password_hash = @password
+# end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+for i in 1..5
+	User.create(first_name: "John#{i}", last_name: "Smith#{i}", email_address: "john@smith.com", about_me: "Hey I'm John!", username: "jsmith#{i}", password: "password", user_type: "standard", city: "San Francisco", state: "CA")
+end
+
+for i in 1..5
+	Business.create(name: "Cool Gym#{i}", business_type: "Gym", address: "201 Van Ness Avenue", city: "San Francisco", state: "CA", zipcode: "94119", phone: 4156577565, website: "http://google.com", availability: "Always open", description: "We are the coolest gym around! Come join us!")
+end
+
+for i in 1..5
+	Review.create(user_id: i, bus_id: i, star_rating: i, review_text: "This place was awesome! Really loved it and thought the staff was really super nice!")
+end
