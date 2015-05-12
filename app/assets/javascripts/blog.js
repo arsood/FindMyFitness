cf1b572007c32180c3d104ebf01a1998dc478e2d
Arun Sood <arsood@gmail.com>
2015-05-12 13:36:19 -0700

    Follow users from profile

--- app/assets/javascripts/blog.js
+++ app/assets/javascripts/blog.js
@@ -73,41 +73,6 @@ if ($("#page_id").length && $("#page_id").val() === "show_post") {
 		});
 	}
 
-	//Process click for following a user
-
-	$(document).on("click", "#follow-user-link", function(event) {
-		event.preventDefault();
-
-		var that = this;
-
-		$.ajax({
-			url: "/followers/add",
-			type: "POST",
-			data: {
-				owner_id: $(this).attr("data-id"),
-				authenticity_token: $("input[name=authenticity_token]").val()
-			},
-			success: function(data) {
-				if (data.result === "ok") {
-					if (data.state === "added") {
-						$("#follow-confirm-title").html("You are now following this user");
-						$("#follow-confirm-text").html("Success! You are now following this user's blog posts.");
-						$("#follow-user-link").html("Unfollow " + $(that).attr("data-name"));
-						$("#follow-confirm-modal").modal("show");
-					} else {
-						$("#follow-confirm-title").html("You are now unsubscribed");
-						$("#follow-confirm-text").html("You are now unsubscribed from this user's posts.");
-						$("#follow-user-link").html("Follow " + $(that).attr("data-name"));
-						$("#follow-confirm-modal").modal("show");
-					}
-				}
-			},
-			error: function() {
-				alert("There was an error processing your request.");
-			}
-		});
-	});
-
 //Close condition that has to be post show page
 } });
 
@@ -145,4 +110,39 @@ $(document).on("click", ".blog-like-button", function(event) {
 			}
 		});
 	}
+});
+
+//Process click for following a user
+
+$(document).on("click", "#follow-user-link", function(event) {
+	event.preventDefault();
+
+	var that = this;
+
+	$.ajax({
+		url: "/followers/add",
+		type: "POST",
+		data: {
+			owner_id: $(this).attr("data-id"),
+			authenticity_token: $("input[name=authenticity_token]").val()
+		},
+		success: function(data) {
+			if (data.result === "ok") {
+				if (data.state === "added") {
+					$("#follow-confirm-title").html("You are now following this user");
+					$("#follow-confirm-text").html("Success! You are now following this user.");
+					$("#follow-user-link").html("Unfollow " + $(that).attr("data-name"));
+					$("#follow-confirm-modal").modal("show");
+				} else {
+					$("#follow-confirm-title").html("You are now unsubscribed");
+					$("#follow-confirm-text").html("You are now unsubscribed from this user.");
+					$("#follow-user-link").html("Follow " + $(that).attr("data-name"));
+					$("#follow-confirm-modal").modal("show");
+				}
+			}
+		},
+		error: function() {
+			alert("There was an error processing your request.");
+		}
+	});
 });
