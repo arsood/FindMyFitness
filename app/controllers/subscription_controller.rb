4003dc93610651d76250ab16cf5929de706170ff
Arun Sood <arsood@gmail.com>
2015-07-25 01:58:34 -0400

    Fix

--- app/controllers/subscription_controller.rb
+++ app/controllers/subscription_controller.rb
@@ -8,37 +8,42 @@ class SubscriptionController < ApplicationController
 	end
 
 	def subscribe
-		customer_result = Braintree::Customer.create(
-			:first_name => session[:first_name],
-			:last_name => session[:last_name],
-			:phone => params[:customer_phone],
-			:payment_method_nonce => params[:payment_method_nonce]
-		)
-
-		if customer_result.success?
-			subscription_result = Braintree::Subscription.create(
-				:payment_method_token => customer_result.customer.credit_cards[0].token,
-				:plan_id => "fmf_business",
-				:id => customer_result.customer.id
+		if User.where(email_address: params[:new_email]).exists?
+			customer_result = Braintree::Customer.create(
+				:first_name => session[:first_name],
+				:last_name => session[:last_name],
+				:phone => params[:customer_phone],
+				:payment_method_nonce => params[:payment_method_nonce]
 			)
 
-			if subscription_result.success?
-				new_user = User.create(email_address: params[:new_email], password: params[:new_password], user_type: "business")
+			if customer_result.success?
+				subscription_result = Braintree::Subscription.create(
+					:payment_method_token => customer_result.customer.credit_cards[0].token,
+					:plan_id => "fmf_business",
+					:id => customer_result.customer.id
+				)
 
-				Subscription.where(user_id: new_user.id).destroy_all
-				
-				Subscription.create(user_id: new_user.id, plan_type: "fmf_business", subscription_status: "active", customer_id: customer_result.customer.id, customer_token: customer_result.customer.credit_cards[0].token, subscription_id: subscription_result.subscription.id)
-				
-				session[:user_id] = new_user.id
-				session[:signing_up] = true
+				if subscription_result.success?
+					new_user = User.create(email_address: params[:new_email], password: params[:new_password], user_type: "business")
 
-				redirect_to "/business-signup"
+					Subscription.where(user_id: new_user.id).destroy_all
+					
+					Subscription.create(user_id: new_user.id, plan_type: "fmf_business", subscription_status: "active", customer_id: customer_result.customer.id, customer_token: customer_result.customer.credit_cards[0].token, subscription_id: subscription_result.subscription.id)
+					
+					session[:user_id] = new_user.id
+					session[:signing_up] = true
+
+					redirect_to "/business-signup"
+				else
+					flash[:error] = "There was an error creating the subscription."
+					redirect_to "/subscribe"
+				end
 			else
-				flash[:error] = "There was an error creating the subscription."
+				flash[:error] = "There was an error with your card. Please try again."
 				redirect_to "/subscribe"
 			end
 		else
-			flash[:error] = "There was an error with your card. Please try again."
+			flash[:error] = "That email has already been taken."
 			redirect_to "/subscribe"
 		end
 	end
